f2a98245bf46f3368a2ec44a60087ee37636c0fa
Ahleeryan <ahleeryan@hotmail.com>
2026-05-10 17:46:28 -0500

    chat box template foundation

--- front-end-biyae/src/App.tsx
+++ front-end-biyae/src/App.tsx
@@ -1,7 +1,3 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "./assets/vite.svg";
-// import heroImg from "./assets/hero.png";
 import "./App.css";
 
 function App() {
@@ -15,6 +11,106 @@ function App() {
         right and left.  
         just examples for now*/}
 
+        <div className="biyae-box">
+          {" "}
+          example of a text from biyae. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Proin elementum massa elit, eu varius nisl tristique
+          laoreet. Cras elementum, quam vel pellentesque cursus, ligula quam
+          fringilla urna, vitae blandit dui justo et ligula. Integer pharetra id
+          ante quis interdum. Mauris hendrerit euismod felis, in suscipit augue
+          malesuada in. Donec id fringilla felis. Donec accumsan tempus dui eu
+          porttitor. Vivamus vitae lorem id magna eleifend porttitor.
+          Suspendisse dolor nulla, varius vel pellentesque sollicitudin, lacinia
+          eu nisl. Nullam id ultrices orci. Donec eu mi lacinia, posuere lacus
+          in, consequat neque. Integer porttitor, diam et tincidunt efficitur,
+          ligula ante molestie nunc, quis molestie massa tortor id turpis.
+          Quisque vitae ligula vel augue semper efficitur quis vitae velit.
+          Vivamus in egestas leo. Mauris posuere tellus euismod, ultricies quam
+          ut, tincidunt justo. Donec bibendum, neque et congue commodo, risus
+          augue eleifend enim, ut tristique arcu nunc a velit. Aliquam vehicula
+          mi non dapibus hendrerit. Fusce id turpis lectus. Donec nec vehicula
+          mauris. Donec ut augue porttitor, elementum felis non, congue arcu.
+          Suspendisse sapien nunc, varius nec varius vel, feugiat faucibus
+          ipsum. Aenean volutpat felis ac nisl pharetra, in dignissim risus
+          interdum. Nunc non efficitur orci, et imperdiet dui. Morbi congue urna
+          sit amet sem consequat condimentum. Etiam convallis nisl in turpis
+          pulvinar rhoncus.
+        </div>
+
+        <div className="user-box">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+          elementum massa elit, eu varius nisl tristique laoreet. Cras
+          elementum, quam vel pellentesque cursus, ligula quam fringilla urna,
+          vitae blandit dui justo et ligula. Integer pharetra id ante quis
+          interdum. Mauris hendrerit euismod felis, in suscipit augue malesuada
+          in. Donec id fringilla felis. Donec accumsan tempus dui eu porttitor.
+          Vivamus vitae lorem id magna eleifend porttitor. Suspendisse dolor
+          nulla, varius vel pellentesque sollicitudin, lacinia eu nisl. Nullam
+          id ultrices orci. Donec eu mi lacinia, posuere lacus in, consequat
+          neque. Integer porttitor, diam et tincidunt efficitur, ligula ante
+          molestie nunc, quis molestie massa tortor id turpis. Quisque vitae
+          ligula vel augue semper efficitur quis vitae velit. Vivamus in egestas
+          leo. Mauris posuere tellus euismod, ultricies quam ut, tincidunt
+          justo. Donec bibendum, neque et congue commodo, risus augue eleifend
+          enim, ut tristique arcu nunc a velit. Aliquam vehicula mi non dapibus
+          hendrerit. Fusce id turpis lectus. Donec nec vehicula mauris. Donec ut
+          augue porttitor, elementum felis non, congue arcu. Suspendisse sapien
+          nunc, varius nec varius vel, feugiat faucibus ipsum. Aenean volutpat
+          felis ac nisl pharetra, in dignissim risus interdum. Nunc non
+          efficitur orci, et imperdiet dui. Morbi congue urna sit amet sem
+          consequat condimentum. Etiam convallis nisl in turpis pulvinar
+          rhoncus.
+        </div>
+        <div className="biyae-box">
+          {" "}
+          example of a text from biyae. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Proin elementum massa elit, eu varius nisl tristique
+          laoreet. Cras elementum, quam vel pellentesque cursus, ligula quam
+          fringilla urna, vitae blandit dui justo et ligula. Integer pharetra id
+          ante quis interdum. Mauris hendrerit euismod felis, in suscipit augue
+          malesuada in. Donec id fringilla felis. Donec accumsan tempus dui eu
+          porttitor. Vivamus vitae lorem id magna eleifend porttitor.
+          Suspendisse dolor nulla, varius vel pellentesque sollicitudin, lacinia
+          eu nisl. Nullam id ultrices orci. Donec eu mi lacinia, posuere lacus
+          in, consequat neque. Integer porttitor, diam et tincidunt efficitur,
+          ligula ante molestie nunc, quis molestie massa tortor id turpis.
+          Quisque vitae ligula vel augue semper efficitur quis vitae velit.
+          Vivamus in egestas leo. Mauris posuere tellus euismod, ultricies quam
+          ut, tincidunt justo. Donec bibendum, neque et congue commodo, risus
+          augue eleifend enim, ut tristique arcu nunc a velit. Aliquam vehicula
+          mi non dapibus hendrerit. Fusce id turpis lectus. Donec nec vehicula
+          mauris. Donec ut augue porttitor, elementum felis non, congue arcu.
+          Suspendisse sapien nunc, varius nec varius vel, feugiat faucibus
+          ipsum. Aenean volutpat felis ac nisl pharetra, in dignissim risus
+          interdum. Nunc non efficitur orci, et imperdiet dui. Morbi congue urna
+          sit amet sem consequat condimentum. Etiam convallis nisl in turpis
+          pulvinar rhoncus.
+        </div>
+
+        <div className="user-box">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+          elementum massa elit, eu varius nisl tristique laoreet. Cras
+          elementum, quam vel pellentesque cursus, ligula quam fringilla urna,
+          vitae blandit dui justo et ligula. Integer pharetra id ante quis
+          interdum. Mauris hendrerit euismod felis, in suscipit augue malesuada
+          in. Donec id fringilla felis. Donec accumsan tempus dui eu porttitor.
+          Vivamus vitae lorem id magna eleifend porttitor. Suspendisse dolor
+          nulla, varius vel pellentesque sollicitudin, lacinia eu nisl. Nullam
+          id ultrices orci. Donec eu mi lacinia, posuere lacus in, consequat
+          neque. Integer porttitor, diam et tincidunt efficitur, ligula ante
+          molestie nunc, quis molestie massa tortor id turpis. Quisque vitae
+          ligula vel augue semper efficitur quis vitae velit. Vivamus in egestas
+          leo. Mauris posuere tellus euismod, ultricies quam ut, tincidunt
+          justo. Donec bibendum, neque et congue commodo, risus augue eleifend
+          enim, ut tristique arcu nunc a velit. Aliquam vehicula mi non dapibus
+          hendrerit. Fusce id turpis lectus. Donec nec vehicula mauris. Donec ut
+          augue porttitor, elementum felis non, congue arcu. Suspendisse sapien
+          nunc, varius nec varius vel, feugiat faucibus ipsum. Aenean volutpat
+          felis ac nisl pharetra, in dignissim risus interdum. Nunc non
+          efficitur orci, et imperdiet dui. Morbi congue urna sit amet sem
+          consequat condimentum. Etiam convallis nisl in turpis pulvinar
+          rhoncus.
+        </div>
         <div className="biyae-box">
           {" "}
           example of a text from biyae. Lorem ipsum dolor sit amet, consectetur
