15ec3a31c2209ef454458409866ee1dd40d84b1a
Ahleeryan <ahleeryan@hotmail.com>
2026-05-12 02:51:28 -0500

    added test sample

--- front-end-biyae/src/App.tsx
+++ front-end-biyae/src/App.tsx
@@ -1,5 +1,14 @@
 import "./App.css";
 
+// import chatExample from "./test-chat.json";
+
+// import type
+// interface Message {
+//   id: number;
+//   role: string;
+//   message: string;
+// }
+
 function App() {
   // const [count, setCount] = useState(0);
 
@@ -8,6 +17,15 @@ function App() {
   // after, we can style things, by the end of this week, we should be staritng the backend.
 
   // avoid force reload upon form submission.
+
+  // const messages = chatExample as Message[];
+
+  // logic.
+  // opens .json. and scans through it chronologicall.
+  // each ID pass, it looks at user.
+  // determin class name based off user.
+  // print the "message" into the div into the chat-box-container.
+  // keep looping until the end?
 
   return (
     <>
@@ -18,156 +36,10 @@ function App() {
         right and left.  
         just examples for now*/}
 
-        <div className="biyae-box">
-          {" "}
-          example of a text from biyae. Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit. Proin elementum massa elit, eu varius nisl tristique
-          laoreet. Cras elementum, quam vel pellentesque cursus, ligula quam
-          fringilla urna, vitae blandit dui justo et ligula. Integer pharetra id
-          ante quis interdum. Mauris hendrerit euismod felis, in suscipit augue
-          malesuada in. Donec id fringilla felis. Donec accumsan tempus dui eu
-          porttitor. Vivamus vitae lorem id magna eleifend porttitor.
-          Suspendisse dolor nulla, varius vel pellentesque sollicitudin, lacinia
-          eu nisl. Nullam id ultrices orci. Donec eu mi lacinia, posuere lacus
-          in, consequat neque. Integer porttitor, diam et tincidunt efficitur,
-          ligula ante molestie nunc, quis molestie massa tortor id turpis.
-          Quisque vitae ligula vel augue semper efficitur quis vitae velit.
-          Vivamus in egestas leo. Mauris posuere tellus euismod, ultricies quam
-          ut, tincidunt justo. Donec bibendum, neque et congue commodo, risus
-          augue eleifend enim, ut tristique arcu nunc a velit. Aliquam vehicula
-          mi non dapibus hendrerit. Fusce id turpis lectus. Donec nec vehicula
-          mauris. Donec ut augue porttitor, elementum felis non, congue arcu.
-          Suspendisse sapien nunc, varius nec varius vel, feugiat faucibus
-          ipsum. Aenean volutpat felis ac nisl pharetra, in dignissim risus
-          interdum. Nunc non efficitur orci, et imperdiet dui. Morbi congue urna
-          sit amet sem consequat condimentum. Etiam convallis nisl in turpis
-          pulvinar rhoncus.
+        {/* <div className="biyae-box">
         </div>
-
         <div className="user-box">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-          elementum massa elit, eu varius nisl tristique laoreet. Cras
-          elementum, quam vel pellentesque cursus, ligula quam fringilla urna,
-          vitae blandit dui justo et ligula. Integer pharetra id ante quis
-          interdum. Mauris hendrerit euismod felis, in suscipit augue malesuada
-          in. Donec id fringilla felis. Donec accumsan tempus dui eu porttitor.
-          Vivamus vitae lorem id magna eleifend porttitor. Suspendisse dolor
-          nulla, varius vel pellentesque sollicitudin, lacinia eu nisl. Nullam
-          id ultrices orci. Donec eu mi lacinia, posuere lacus in, consequat
-          neque. Integer porttitor, diam et tincidunt efficitur, ligula ante
-          molestie nunc, quis molestie massa tortor id turpis. Quisque vitae
-          ligula vel augue semper efficitur quis vitae velit. Vivamus in egestas
-          leo. Mauris posuere tellus euismod, ultricies quam ut, tincidunt
-          justo. Donec bibendum, neque et congue commodo, risus augue eleifend
-          enim, ut tristique arcu nunc a velit. Aliquam vehicula mi non dapibus
-          hendrerit. Fusce id turpis lectus. Donec nec vehicula mauris. Donec ut
-          augue porttitor, elementum felis non, congue arcu. Suspendisse sapien
-          nunc, varius nec varius vel, feugiat faucibus ipsum. Aenean volutpat
-          felis ac nisl pharetra, in dignissim risus interdum. Nunc non
-          efficitur orci, et imperdiet dui. Morbi congue urna sit amet sem
-          consequat condimentum. Etiam convallis nisl in turpis pulvinar
-          rhoncus.
-        </div>
-        <div className="biyae-box">
-          {" "}
-          example of a text from biyae. Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit. Proin elementum massa elit, eu varius nisl tristique
-          laoreet. Cras elementum, quam vel pellentesque cursus, ligula quam
-          fringilla urna, vitae blandit dui justo et ligula. Integer pharetra id
-          ante quis interdum. Mauris hendrerit euismod felis, in suscipit augue
-          malesuada in. Donec id fringilla felis. Donec accumsan tempus dui eu
-          porttitor. Vivamus vitae lorem id magna eleifend porttitor.
-          Suspendisse dolor nulla, varius vel pellentesque sollicitudin, lacinia
-          eu nisl. Nullam id ultrices orci. Donec eu mi lacinia, posuere lacus
-          in, consequat neque. Integer porttitor, diam et tincidunt efficitur,
-          ligula ante molestie nunc, quis molestie massa tortor id turpis.
-          Quisque vitae ligula vel augue semper efficitur quis vitae velit.
-          Vivamus in egestas leo. Mauris posuere tellus euismod, ultricies quam
-          ut, tincidunt justo. Donec bibendum, neque et congue commodo, risus
-          augue eleifend enim, ut tristique arcu nunc a velit. Aliquam vehicula
-          mi non dapibus hendrerit. Fusce id turpis lectus. Donec nec vehicula
-          mauris. Donec ut augue porttitor, elementum felis non, congue arcu.
-          Suspendisse sapien nunc, varius nec varius vel, feugiat faucibus
-          ipsum. Aenean volutpat felis ac nisl pharetra, in dignissim risus
-          interdum. Nunc non efficitur orci, et imperdiet dui. Morbi congue urna
-          sit amet sem consequat condimentum. Etiam convallis nisl in turpis
-          pulvinar rhoncus.
-        </div>
-
-        <div className="user-box">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-          elementum massa elit, eu varius nisl tristique laoreet. Cras
-          elementum, quam vel pellentesque cursus, ligula quam fringilla urna,
-          vitae blandit dui justo et ligula. Integer pharetra id ante quis
-          interdum. Mauris hendrerit euismod felis, in suscipit augue malesuada
-          in. Donec id fringilla felis. Donec accumsan tempus dui eu porttitor.
-          Vivamus vitae lorem id magna eleifend porttitor. Suspendisse dolor
-          nulla, varius vel pellentesque sollicitudin, lacinia eu nisl. Nullam
-          id ultrices orci. Donec eu mi lacinia, posuere lacus in, consequat
-          neque. Integer porttitor, diam et tincidunt efficitur, ligula ante
-          molestie nunc, quis molestie massa tortor id turpis. Quisque vitae
-          ligula vel augue semper efficitur quis vitae velit. Vivamus in egestas
-          leo. Mauris posuere tellus euismod, ultricies quam ut, tincidunt
-          justo. Donec bibendum, neque et congue commodo, risus augue eleifend
-          enim, ut tristique arcu nunc a velit. Aliquam vehicula mi non dapibus
-          hendrerit. Fusce id turpis lectus. Donec nec vehicula mauris. Donec ut
-          augue porttitor, elementum felis non, congue arcu. Suspendisse sapien
-          nunc, varius nec varius vel, feugiat faucibus ipsum. Aenean volutpat
-          felis ac nisl pharetra, in dignissim risus interdum. Nunc non
-          efficitur orci, et imperdiet dui. Morbi congue urna sit amet sem
-          consequat condimentum. Etiam convallis nisl in turpis pulvinar
-          rhoncus.
-        </div>
-        <div className="biyae-box">
-          {" "}
-          example of a text from biyae. Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit. Proin elementum massa elit, eu varius nisl tristique
-          laoreet. Cras elementum, quam vel pellentesque cursus, ligula quam
-          fringilla urna, vitae blandit dui justo et ligula. Integer pharetra id
-          ante quis interdum. Mauris hendrerit euismod felis, in suscipit augue
-          malesuada in. Donec id fringilla felis. Donec accumsan tempus dui eu
-          porttitor. Vivamus vitae lorem id magna eleifend porttitor.
-          Suspendisse dolor nulla, varius vel pellentesque sollicitudin, lacinia
-          eu nisl. Nullam id ultrices orci. Donec eu mi lacinia, posuere lacus
-          in, consequat neque. Integer porttitor, diam et tincidunt efficitur,
-          ligula ante molestie nunc, quis molestie massa tortor id turpis.
-          Quisque vitae ligula vel augue semper efficitur quis vitae velit.
-          Vivamus in egestas leo. Mauris posuere tellus euismod, ultricies quam
-          ut, tincidunt justo. Donec bibendum, neque et congue commodo, risus
-          augue eleifend enim, ut tristique arcu nunc a velit. Aliquam vehicula
-          mi non dapibus hendrerit. Fusce id turpis lectus. Donec nec vehicula
-          mauris. Donec ut augue porttitor, elementum felis non, congue arcu.
-          Suspendisse sapien nunc, varius nec varius vel, feugiat faucibus
-          ipsum. Aenean volutpat felis ac nisl pharetra, in dignissim risus
-          interdum. Nunc non efficitur orci, et imperdiet dui. Morbi congue urna
-          sit amet sem consequat condimentum. Etiam convallis nisl in turpis
-          pulvinar rhoncus.
-        </div>
-
-        <div className="user-box">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-          elementum massa elit, eu varius nisl tristique laoreet. Cras
-          elementum, quam vel pellentesque cursus, ligula quam fringilla urna,
-          vitae blandit dui justo et ligula. Integer pharetra id ante quis
-          interdum. Mauris hendrerit euismod felis, in suscipit augue malesuada
-          in. Donec id fringilla felis. Donec accumsan tempus dui eu porttitor.
-          Vivamus vitae lorem id magna eleifend porttitor. Suspendisse dolor
-          nulla, varius vel pellentesque sollicitudin, lacinia eu nisl. Nullam
-          id ultrices orci. Donec eu mi lacinia, posuere lacus in, consequat
-          neque. Integer porttitor, diam et tincidunt efficitur, ligula ante
-          molestie nunc, quis molestie massa tortor id turpis. Quisque vitae
-          ligula vel augue semper efficitur quis vitae velit. Vivamus in egestas
-          leo. Mauris posuere tellus euismod, ultricies quam ut, tincidunt
-          justo. Donec bibendum, neque et congue commodo, risus augue eleifend
-          enim, ut tristique arcu nunc a velit. Aliquam vehicula mi non dapibus
-          hendrerit. Fusce id turpis lectus. Donec nec vehicula mauris. Donec ut
-          augue porttitor, elementum felis non, congue arcu. Suspendisse sapien
-          nunc, varius nec varius vel, feugiat faucibus ipsum. Aenean volutpat
-          felis ac nisl pharetra, in dignissim risus interdum. Nunc non
-          efficitur orci, et imperdiet dui. Morbi congue urna sit amet sem
-          consequat condimentum. Etiam convallis nisl in turpis pulvinar
-          rhoncus.
-        </div>
+        </div> */}
       </section>
       <section id="text-input">
         {/* todo: make it so that when overflow happens, it pushes upwards and doesn't 
