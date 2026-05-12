00ce21d625af674d80deac28e2397a60144873e5
Ahleeryan <ahleeryan@hotmail.com>
2026-05-12 03:26:47 -0500

    proof of concept done. clean later

--- front-end-biyae/src/App.tsx
+++ front-end-biyae/src/App.tsx
@@ -1,13 +1,5 @@
 import "./App.css";
-
-// import chatExample from "./test-chat.json";
-
-// import type
-// interface Message {
-//   id: number;
-//   role: string;
-//   message: string;
-// }
+import { ChatBoxContainer } from "./ChatBoxContainer";
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -18,29 +10,10 @@ function App() {
 
   // avoid force reload upon form submission.
 
-  // const messages = chatExample as Message[];
-
-  // logic.
-  // opens .json. and scans through it chronologicall.
-  // each ID pass, it looks at user.
-  // determin class name based off user.
-  // print the "message" into the div into the chat-box-container.
-  // keep looping until the end?
-
   return (
     <>
       {/* big container */}
-
-      <section id="chat-box-container">
-        {/* section of chat containers multiple.
-        right and left.  
-        just examples for now*/}
-
-        {/* <div className="biyae-box">
-        </div>
-        <div className="user-box">
-        </div> */}
-      </section>
+      <ChatBoxContainer />
       <section id="text-input">
         {/* todo: make it so that when overflow happens, it pushes upwards and doesn't 
           go downwards */}
---
+++ front-end-biyae/src/ChatBoxContainer.tsx
@@ -0,0 +1,39 @@
+import chatExample from "./test-chat.json";
+
+// import type
+interface Message {
+  id: number;
+  role: string;
+  message: string;
+}
+
+export function ChatBoxContainer() {
+  const messages = chatExample as Message[];
+
+  return (
+    <section id="chat-box-container">
+      {/* section of chat containers multiple.
+        right and left.  
+        just examples for now*/}
+
+      {/* <div className="biyae-box">
+        </div>
+        <div className="user-box">
+        </div> */}
+
+      {messages
+        .sort((a, b) => a.id - b.id)
+        .map((msg) => {
+          const messageClass = msg.role === "User" ? "user-box" : "biyae-box";
+
+          return (
+            <div key={msg.id} className={`message ${messageClass}`}>
+              <p>{msg.role}:</p>
+              <p className="message-content">{msg.message}</p>
+            </div>
+          );
+        })}
+      {/* todo: clean all of this up. encapsluate it outside of return for cleanliness. */}
+    </section>
+  );
+}
