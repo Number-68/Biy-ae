50a05955394bc3814a4fbacd7ba54f050dff82fc
Ahleeryan <ahleeryan@hotmail.com>
2026-05-12 20:17:23 -0500

    cleaned up component organization

--- front-end-biyae/src/App.tsx
+++ front-end-biyae/src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
 import "./App.css";
 import { ChatBoxContainer } from "./ChatBoxContainer";
+import { InputBoxContainer } from "./TextInputContainer";
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -11,40 +11,11 @@ function App() {
 
   // avoid force reload upon form submission.
 
-  // define handlers
-  const [input, setInput] = useState("");
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key === "Enter" && !e.shiftKey) {
-      e.preventDefault();
-      sendMessage();
-    }
-  };
-
-  //send the message
-  const sendMessage = () => {
-    console.log(input);
-    //for now, log it to console.
-    setInput("");
-  };
-
   return (
     <>
       {/* big container */}
       <ChatBoxContainer />
-      <section id="text-input">
-        {/* todo: make it so that when overflow happens, it pushes upwards and doesn't 
-          go downwards */}
-
-        <textarea
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onKeyDown={handleKeyDown}
-        />
-        {/* small random happensance. when entering nothing, it still sends. ensure to clear up that so that we don't have any blank inputs. */}
-
-        <button type="submit">send!</button>
-      </section>
+      <InputBoxContainer />
     </>
   );
 }
