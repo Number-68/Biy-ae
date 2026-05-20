8220d3f7dcff8799ed82ec81dc53b0bf8ed0930f
Ahleeryan <ahleeryan@hotmail.com>
2026-05-20 16:16:19 -0500

    cleaned more annotations

--- front-end-biyae/src/App.tsx
+++ front-end-biyae/src/App.tsx
@@ -5,12 +5,6 @@ import { InputBoxContainer } from "./TextInputContainer";
 function App() {
   // const [count, setCount] = useState(0);
 
-  // todo;where i last ended off
-  // I'mgoingto just build all of the components right now. and then style later. next, we have to do the header nav, and then the conversation history pullout. that should be it, I think?
-  // after, we can style things, by the end of this week, we should be staritng the backend.
-
-  // avoid force reload upon form submission.
-
   return (
     <>
       {/* big container */}
