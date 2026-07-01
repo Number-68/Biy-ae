635a19aaf31ad30a06ad35483593a64df265b749
ahleeryan <ahleeryan@hotmail.com>
2026-07-01 12:46:53 -0500

    added signup page.

---
+++ front-end-biyae/src/Pages/SignUp/signup.tsx
@@ -0,0 +1,9 @@
+function Signup() {
+  return (
+    <>
+      <div>hello! you have reached the signup page.</div>
+    </>
+  );
+}
+
+export default Signup;
--- front-end-biyae/src/routes.tsx
+++ front-end-biyae/src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./Pages/App/App.tsx";
 import Login from "./Pages/Login/login.tsx";
+import Signup from "./Pages/SignUp/signup.tsx";
 
 export const router = createBrowserRouter([
   {
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
   },
 ]);
