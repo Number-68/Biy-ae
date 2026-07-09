d942af41d76cc645fd8bca148d75ae6875acf6cf
ahleeryan <ahleeryan@hotmail.com>
2026-07-09 09:53:09 -0500

    added login and signup

--- front-end-biyae/src/Pages/Login/login.tsx
+++ front-end-biyae/src/Pages/Login/login.tsx
@@ -2,6 +2,29 @@ function Login() {
   return (
     <>
       <div>hello, this is the login page.</div>
+      <form>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Enter your username"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter your password"
+          />
+        </div>
+
+        <button type="submit">Enter</button>
+      </form>
     </>
   );
 }
--- front-end-biyae/src/Pages/SignUp/signup.tsx
+++ front-end-biyae/src/Pages/SignUp/signup.tsx
@@ -2,6 +2,49 @@ function Signup() {
   return (
     <>
       <div>hello! you have reached the signup page.</div>
+
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter your email"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Enter your username"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter your password"
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            placeholder="Confirm your password"
+          />
+        </div>
+
+        <button type="submit">Sign Up</button>
+      </form>
     </>
   );
 }
